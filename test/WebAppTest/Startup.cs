a4dab804d45b99ae6816338fd5c0a712f86026c3
万里涛 <wanlitao_3055601@outlook.com>
2019-10-10 20:06:25 +0800

    adjust WebApp HostBuilder

--- test/WebAppTest/Startup.cs
+++ test/WebAppTest/Startup.cs
@@ -27,7 +27,7 @@ namespace WebAppTest
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
