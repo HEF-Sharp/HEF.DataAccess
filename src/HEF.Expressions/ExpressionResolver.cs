55e76e84f6c7193d0ca51650075270ad35280a44
万里涛 <wanlitao_3055601@outlook.com>
2019-11-29 19:29:43 +0800

    add expressions project

---
+++ src/HEF.Expressions/ExpressionResolver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace HEF.Expressions
+{
+    public abstract class ExpressionResolver : ExpressionVisitor
+    {
+        public override Expression Visit(Expression expression)
+        {
+            if (expression == null)
+                throw new ArgumentNullException(nameof(expression));
+            
+            if (IsResolveNodeType(expression.NodeType))
+                return base.Visit(expression);
+
+            throw new NotSupportedException($"The LINQ expression node of type {expression.NodeType} is not supported");
+        }
+
+        protected abstract bool IsResolveNodeType(ExpressionType nodeType);
+    }
+}
