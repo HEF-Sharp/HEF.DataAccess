09b7165467d84bafb17853667934626876dfe52d
万里涛 <wanlitao_3055601@outlook.com>
2019-12-12 21:54:26 +0800

    add Expression resolver

---
+++ src/HEF.Expressions.Sql/ExpressionSqlResolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace HEF.Expressions.Sql
+{
+    public class ExpressionSqlResolver : ExpressionResolver
+    {
+        protected override bool IsResolveNodeType(ExpressionType nodeType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/HEF.Expressions/ExpressionTypeExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq.Expressions;
+
+namespace HEF.Expressions
+{
+    public static class ExpressionTypeExtensions
+    {
+        public static bool IsLambda(this Expression expr)
+        {
+            return expr.NodeType == ExpressionType.Lambda;
+        }
+
+        public static bool IsMethodCall(this Expression expr)
+        {
+            return expr.NodeType == ExpressionType.Call;
+        }
+
+        public static bool IsMemberAccess(this Expression expr)
+        {
+            return expr.NodeType == ExpressionType.MemberAccess;
+        }
+
+        public static bool IsConstant(this Expression expr)
+        {
+            return expr.NodeType == ExpressionType.Constant;
+        }
+
+        public static bool IsParameter(this Expression expr)
+        {
+            return expr.NodeType == ExpressionType.Parameter;
+        }
+
+        public static bool IsLogicOperation(this Expression expr)
+        {
+            return expr.NodeType == ExpressionType.And ||
+                expr.NodeType == ExpressionType.AndAlso ||
+                expr.NodeType == ExpressionType.Or ||
+                expr.NodeType == ExpressionType.OrElse;
+        }
+
+        public static bool IsCompareOperation(this Expression expr)
+        {
+            return expr.NodeType == ExpressionType.LessThan ||
+                expr.NodeType == ExpressionType.LessThanOrEqual ||
+                expr.NodeType == ExpressionType.GreaterThan ||
+                expr.NodeType == ExpressionType.GreaterThanOrEqual ||
+                expr.NodeType == ExpressionType.Equal ||
+                expr.NodeType == ExpressionType.NotEqual;
+        }
+
+        public static bool IsMathOperation(this Expression expr)
+        {
+            return expr.NodeType == ExpressionType.Add ||
+                expr.NodeType == ExpressionType.AddChecked ||
+                expr.NodeType == ExpressionType.Subtract ||
+                expr.NodeType == ExpressionType.SubtractChecked ||
+                expr.NodeType == ExpressionType.Multiply ||
+                expr.NodeType == ExpressionType.MultiplyChecked ||
+                expr.NodeType == ExpressionType.Divide ||
+                expr.NodeType == ExpressionType.Modulo ||
+                expr.NodeType == ExpressionType.Power;
+        }
+    }
+}
